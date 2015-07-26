e6f78469f3f4a11e50eda649ef02a3e9d86319d9
rurtubiac <randy.urtubia@inacapmail.cl>
2015-07-25 23:45:12 -0300

    added MethodOverload

---
+++ VSfAB_Tutorial_01/MethodOverloads.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSfAB_Tutorial_01
+{
+    class MethodOverloads
+    {
+        public void methodOverloads() 
+        {
+            string myValue = superSecretFormula();
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+
+        private string superSecretFormula()
+        {
+            return "Hello World";
+        }
+    }
+}
