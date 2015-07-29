f7f0ee0a5e23811cb53a7a262e6a27946edb3f5d
rurtubiac <randy.urtubia@inacapmail.cl>
2015-07-28 23:50:59 -0300

    added several formats

--- VSfAB_Tutorial_01/Strings.cs
+++ VSfAB_Tutorial_01/Strings.cs
@@ -21,7 +21,21 @@ namespace VSfAB_Tutorial_01
 
             string myString = string.Format("{0}!", "Bonzai");
 
-            string myString3 = string.Format();
+            //Calling parameters
+            string myString3 = string.Format("Make: {0} (Model:{1})", "BMW", "760li");
+
+            //Currency format
+            string myString4 = string.Format("{0:C}", 123.45);
+
+            //Number format
+            string myString5 = string.Format("{0:N}", 123456789);
+
+            //Percentage format
+            string myString6 = string.Format("{0:P}", .123);
+
+            //Custom formats
+            //the # sign is used 
+
 
             Console.WriteLine(myString);
             Console.ReadLine();
